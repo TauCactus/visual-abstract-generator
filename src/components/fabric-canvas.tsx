417211d4c4150d2cba7d5435686732b5ca1e70e1
Tau Sand <tau.sand@cactusglobal.com>
2023-10-06 12:25:25 +0200

    only add canvas on client side

--- src/components/fabric-canvas.tsx
+++ src/components/fabric-canvas.tsx
@@ -1,11 +1,16 @@
 import { fabric } from "fabric";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 export const FabricCanvas = (props: {
   json: unknown;
   height: number;
   width: number;
 }) => {
+  const [isSSR, setIsSSR] = useState(true);
   useEffect(() => {
+    if(isSSR) {
+      setIsSSR(false);
+      return;
+    }
     const element: any = document.querySelector("#fabric-canvas");
     const instance = new fabric.Canvas(element, {
       width: props.width,
@@ -16,6 +21,6 @@ export const FabricCanvas = (props: {
       preserveObjectStacking: true,
     });
     instance.loadFromJSON(props.json, () => {});
-  }, [props.json]);
+  }, [props.json,isSSR]);
   return <canvas id="fabric-canvas"></canvas>;
 };
