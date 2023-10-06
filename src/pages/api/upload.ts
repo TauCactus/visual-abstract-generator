39c423547371497417461bfea1119d837782b4d5
Tau Sand <tau.sand@cactusglobal.com>
2023-10-06 22:26:24 +0200

    added titan api and more

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -2,6 +2,9 @@ import type {NextApiRequest, NextApiResponse} from "next";
 import {adjectives, animals, uniqueNamesGenerator} from "unique-names-generator";
 import {Redis} from "@upstash/redis";
 import formidable from 'formidable';
+import {promises as fs} from 'fs';
+
+import {fetchTitan, TitanFetchResponse, uploadFileToTitan} from "@/server/titan/titan-api"
 
 const redis = new Redis({
     url: process.env.redis_host!,
@@ -29,16 +32,35 @@ export default async function handler(
     res: NextApiResponse<Data>,
 ) {
     const form = formidable({});
-    const [fields, files] = await form.parse(req);
-    console.log(fields, files);
+    const [_, files] = await form.parse(req);
     const id = uniqueNamesGenerator({
         style: 'lowerCase',
         separator: '-',
         length: 3,
         dictionaries: [adjectives, adjectives, animals]
     });
+    if (!files.file?.[0]) {
+        throw new Error("No file uploaded");
+    }
+    const file = files.file[0];
+    const buffer = await fs.readFile(file.filepath)
+    const blob = new Blob([buffer]);
+    const response = await uploadFileToTitan(blob, file.originalFilename!, {id: id});
 
-    await redis.set(id, {name: "empty"});
-    await sleep(3000);
+
+    let fetchResponse: TitanFetchResponse | null = null;
+    while (!(fetchResponse?.status ?? false)) {
+        await sleep(2000);
+        fetchResponse = await fetchTitan(response.request_id);
+    }
+    console.log("get response", fetchResponse);
+    if(!fetchResponse || !fetchResponse.status) {
+        throw new Error("Something is wrong");
+    }
+    const result = await fetch(fetchResponse.data.url);
+    const json = await result.json();
+    console.log(json)
+    await redis.set(id, {name: "empty", json});
+
     res.status(200).json({host: process.env.DB_HOST!, id, imageUrl: "/example-abstract.png"});
 }
--- src/server/titan/titan-api.ts
+++ src/server/titan/titan-api.ts
@@ -1,43 +1,70 @@
-async function uploadFile(file: File, metaData: unknown) {
-  if (file.type !== "application/pdf") {
-    throw new Error("Only pdfs allowed");
-  }
-  const url = "your_url";
-  const bearertoken = "your_bearer_token";
+import {z} from "zod";
 
-  const formData = new FormData();
-  formData.append("file", file);
-  const jobConfig = JSON.stringify({
-    extract: true,
-    doc_class: true,
-    fos: true,
-  });
-  const metadata = JSON.stringify(metaData);
+const submitUrl = `${process.env.titan_host}/submit`;
+const fetchUrl = (requestId: string)=> `${process.env.titan_host}/fetch?request_id=${requestId}`;
+const bearertoken = process.env.titan_token!;
 
-  const headers = new Headers({
-    Authorization: `Bearer ${bearertoken}`,
-  });
-
-  formData.append("job_config", jobConfig);
-  formData.append("metadata", metadata);
-
-  const requestOptions: RequestInit = {
-    method: "POST",
-    headers,
-    body: formData,
-  };
-
-  fetch(url, requestOptions)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json(); // or response.text() if the response is not JSON
-    })
-    .then((data) => {
-      console.log(data); // Handle the response data here
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
+export async function uploadFileToTitan(file: Blob, filename: string, metaData: unknown) {
+    const formData = new FormData();
+    formData.append("file", file, filename);
+    const jobConfig = JSON.stringify({
+        extract: true,
+        doc_class: true,
+        fos: true,
     });
+    const metadata = JSON.stringify(metaData);
+
+    const headers = new Headers({
+        Authorization: `Bearer ${bearertoken}`,
+    });
+
+    formData.append("job_config", jobConfig);
+    formData.append("metadata", metadata);
+
+    const requestOptions: RequestInit = {
+        method: "POST",
+        headers,
+        body: formData,
+    };
+
+    const response = await fetch(submitUrl, requestOptions);
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const json = await response.json();
+    return TitanUploadResponse.parse(json);
 }
+
+export async function fetchTitan(requestId: string) {
+    const headers = new Headers({
+        Authorization: `Bearer ${bearertoken}`,
+    });
+    const response = await fetch(fetchUrl(requestId), {headers});
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const json = await response.json();
+    return TitanFetchResponse.parse(json);
+}
+
+const TitanUploadResponse = z.object({
+    request_id: z.string(),
+    status: z.boolean(),
+    message: z.string(),
+});
+
+
+const TitanFetchResponse = z.union([z.object({
+    request_id: z.string(),
+    status: z.literal(false),
+    metadata: z.object({id: z.string()}),
+}), z.object({
+    status: z.literal(true),
+    request_id: z.string(),
+    metadata: z.object({id: z.string()}),
+    data: z.object({
+        url: z.string()
+    })
+})]);
+
+export type TitanFetchResponse = z.infer<typeof TitanFetchResponse>
